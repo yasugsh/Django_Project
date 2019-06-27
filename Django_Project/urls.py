a5c0e8e813f6aef421cbdf93563b16383db8e70e
python <976093462@qq.com>
2019-06-27 21:39:29 +0800

    配置商城Logo的url

--- Django_Project/urls.py
+++ Django_Project/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^favicon.ico$', RedirectView.as_view(url=r'static/favicon.ico')),
     url(r'^search/', include('haystack.urls')),
     url(r'^', include('users.urls', namespace='users')),
     url(r'^', include('contents.urls', namespace='contents')),
